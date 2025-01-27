9fc907e31e2af9a7e573c5b157f744bffa2f0b72
kbr_1218 <kbr_uni@naver.com>
2025-01-27 23:46:02 +0900

    [FIX] prompt templates 위치 변경

--- api/recommend.py
+++ api/recommend.py
@@ -5,7 +5,7 @@ from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
-from setup import movies_vectorstore
+from setup import movies_vectorstore, load_template_from_yaml
 from config import GEMINI_API_KEY, OPENAI_API_KEY
 
 # StructuredOutputParser 사용
@@ -39,7 +39,6 @@ def load_gpt():
     )
     print(">>>>>>> GPT loaded from recommend chain...")
     return model
-# system_instruction = """you are a movie-recommendation chatbot. you must answer based on given data."""
 
 # 검색기 생성
 multiquery_chain_retriever = MultiQueryRetriever.from_llm(
@@ -53,38 +52,9 @@ multiquery_chain_retriever = MultiQueryRetriever.from_llm(
     llm = load_gemini()
 )
 
-
-# 프롬프트 템플릿 설정
-recommend_chain_template = """
-You are a movie-recommendation chatbot.
-You must only answer based on the given context.
-Do not generate answers that are not directly supported by the context.
-사용자의 요청에 따라 추천할 VOD 콘텐츠 **asset_id의 리스트**를 반환하세요.
-
-**중요**:
-- 추천 리스트에는 반드시 **[Context]**에서 제공된 문서만 포함해야 합니다.
-- **[Context]**에 없는 문서를 절대 생성하거나 포함하지 마세요.
-- **[Context]**에 적절한 추천이 없을 경우, 빈 리스트를 반환하세요.
-
-응답은 JSON 형식으로 **오직 추천된 콘텐츠의 asset_id의 리스트**만 포함해야 합니다.
-{recommend_chain_format_instructions}
----
-예제 출력 형식:
-```json
-{{"candidates": [1, 10, 31, 89, 135, 180]}}```
-(만약 적절한 추천이 없을 경우)
-```json
-{{"candidates": []}}```
-
-[사용자 입력과 사용자 입력값의 유형]:
-{user_input}
-
-[Context]:
-{recommend_chain_retriever}
-
-[Answer]:
-"""
-recommend_chain_prompt = ChatPromptTemplate.from_template(recommend_chain_template,
+# template 불러오기
+recommend_template = load_template_from_yaml("./prompts/recommend_template.yaml")
+recommend_chain_prompt = ChatPromptTemplate.from_template(recommend_template,
                                                           partial_variables={'recommend_chain_format_instructions': recommend_chain_format_instructions})
 recommend_chain_llm = load_gemini()
 
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from langchain_teddynote import logging
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from config import VECTORSTORE_PATH_MOVIE, VECTORSTORE_PATH_VIEW_100, LOGGING_NAME
+import yaml
 
 # langsmith 추적 설정
 logging.langsmith(LOGGING_NAME)
@@ -21,3 +22,9 @@ except Exception as e:
     print(f"❌ Error Loading VectorStore: {e}")
     movies_vectorstore = None  # 벡터스토어 로딩 실패 시 예외 처리는 여기에
     views_vectorstore = None
+
+# yaml에서 template 불러오는 함수
+def load_template_from_yaml(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = yaml.safe_load(file)
+    return data['template']
