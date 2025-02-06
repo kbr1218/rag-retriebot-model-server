1e075b5baa9af8ae48334050a3a0988f60a16ca0
asoonap <namminju0328@gmail.com>
2025-02-07 00:53:34 +0900

    [FEAT] search chain 추가, 추천체인주석처리되어있음

---
+++ main.py
@@ -0,0 +1,134 @@
+# main.py
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from chain.recommend import recommend_chain
+from chain.post_recommend import post_recommend_chain
+from chain.search import search_chain
+# from chain.wrapup import wrapup_chain
+from setup import views_vectorstore
+from functions.user_utils import find_user_vectors
+from functions.add_views import add_view_to_vectorstore
+from functions.fetch_movie_details import fetch_movie_details
+from functions.convert_to_json import convert_to_json
+import json
+
+app = FastAPI()
+
+# 사용자 입력값 데이터 모델 정의
+class UserInput(BaseModel):
+  user_input: str
+
+# 시청기록 저장용 데이터 모델 정의
+class WatchInput(BaseModel):
+  asset_id: str
+  runtime: float
+
+# 사용자 시청기록 데이터 변수
+user_data_cache = {}
+
+@app.get('/')
+def load_root():
+  return {'hi': "model server is running(port: 8000)💭"}
+
+
+# 사용자 ID 확인 및 시청기록 검색
+@app.post('/{userid}/api/connect')
+def check_user_id(userid: str):
+  try:
+    # 벡터스토어에서 user_id 검색
+    user_vectors = find_user_vectors(userid)
+    if user_vectors:
+      # 사용자의 데이터를 전역 변수에 저장
+      user_data_cache[userid] = user_vectors
+      return {"message": f"{userid}", "records_found": len(user_vectors)}        # 200
+    else:
+      raise HTTPException(status_code=404, detail="user not found")              # 404
+  except Exception as e:
+      raise HTTPException(status_code=500, detail=f"Error checking user ID: {str(e)}")  # 500
+
+# # 추천요청 체인
+# @app.post('/{userid}/api/recommend')
+# def load_recommend(userid: str, user_input: UserInput):
+#   print("recommend API 실행 시작 여기부터")
+
+#   # 사용자 벡터 캐시 확인
+#   if userid not in user_data_cache:
+#     raise HTTPException(status_code=400, detail="사용자를 찾을 수 없음 (/api/connect 먼저 호출하쇼)")
+
+#   try:
+#     # VOD 콘텐츠의 후보를 선정하는 체인 실행
+#     response = recommend_chain.invoke(user_input.user_input)
+#     candidate_asset_ids = response.get("candidates", [])
+
+#     if not candidate_asset_ids:
+#       raise HTTPException(status_code=500, detail="추천할 VOD 후보가 없습니다.")
+
+#     candidate_movies = fetch_movie_details(candidate_asset_ids)
+#     print(f">>>>>>>>> {candidate_asset_ids}")
+
+#     # 사용자가 시청한 VOD의 asset_id를 변수에 저장
+#     watched_movies_asset_ids = [doc.metadata["asset_id"] for doc, _ in user_data_cache[userid]]
+#     watched_movies = fetch_movie_details(watched_movies_asset_ids)
+#     print(f">>>>>>>>> {watched_movies_asset_ids}")
+
+#     # 사용자 시청기록을 사용하여 후보 VOD 중 5개 선정
+#     final_recommendation = post_recommend_chain.invoke(
+#       {"user_input": user_input.user_input,
+#        "candidate_movies": candidate_movies,
+#        "watched_movies": watched_movies
+#       }
+#     )
+
+#     # 선정된 5개의 영화들로 wrapup chain 실행
+#     final_response = wrapup_chain.invoke({"final_recommendations": final_recommendation["final_recommendations"]})
+
+#     return candidate_asset_ids
+#   except Exception as e:
+#     raise HTTPException(status_code=500, detail = f"recommend API error: {str(e)}")  # 500
+  
+
+
+  
+
+# search요청 체인-보리코드 -------------------------------------------------------------------------------------------------------------
+@app.post('/{userid}/api/search')
+def search_invoke(userid: str, user_input: UserInput):
+  print("search API 실행 시작 여기부터")
+
+  # 사용자 벡터 캐시 확인
+  if userid not in user_data_cache:
+    raise HTTPException(status_code=400, detail="사용자를 찾을 수 없음 (/api/connect 먼저 호출하쇼)")
+
+  try:
+    response = search_chain.invoke(user_input.user_input)
+    raw_results = fetch_movie_details(response["asset_id"])
+
+    print(f"\n>>>>>>>>> raw_results HERE: \n{raw_results}")
+    # 8) 클라이언트에게 전송할 수 있도록 JSON 형식으로 변환- 보리코드----
+    results = {
+      str(index + 1): convert_to_json(movie_data["page_content"])
+      for index, (_, movie_data) in enumerate(raw_results["movie_details"].items())
+    }
+
+    return {
+      "movies": results,
+      "answer": response["reason"]
+    }
+  except Exception as e:
+    raise HTTPException(status_code=500, detail = f"search API error: {str(e)}")  # 500
+
+
+
+# 시청기록 추가
+@app.post('/{user_id}/api/watch')
+def add_watch_record(user_id: str, watch_input: WatchInput):
+  print(f"\n------------- WATCH API 실행 -------------")
+  asset_id = watch_input.asset_id
+  runtime = watch_input.runtime
+
+  try:
+    # 새로운 시청기록 추가
+    add_view_to_vectorstore(user_id, asset_id, runtime)
+    return {"message": f"시청기록 추가 완료 >> {user_id} - {asset_id}"}
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=f"시청기록 추가 실패: {str(e)}")
